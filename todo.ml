e63df1f00bd8cff74a2177cb39cd40cc76c803b6
Nathan Sauret <nathan@nc-ass-vip.sdv.fr>
2024-09-26 15:05:29 +0200

    Ennemies added correctly

--- todo.ml
+++ todo.ml
@@ -5,9 +5,10 @@
 [X] - Ennemies are destroying coins, bad bad!
 [X] - Add move counter to the screen
 [X] - Get last ennemy frame before dying
-[ ] - Add ennemies sprites
+[X] - Add ennemies sprites
 [ ] - Condition to check if '\n' in the is_rectangular
 [ ] - Valgrind error when empty line anywhere in the map except at the begining
+[ ] - Add '\n' after Error instead of after the name of the error
 
 ???
 [ ] - Konami code
